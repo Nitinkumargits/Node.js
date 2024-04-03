1fa592dbf1107c64734a07b0a1f718571ccf5bc0
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-04-03 20:13:27 +0530

    build a simple api

--- 01.Node-fram/index.js
+++ 01.Node-fram/index.js
@@ -27,12 +27,17 @@ const url = require("url");
 */
 //////////////////////////////////////////////
 /////----Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
   } else if (pathName === "/product") {
     res.end("This is the product");
   } else {

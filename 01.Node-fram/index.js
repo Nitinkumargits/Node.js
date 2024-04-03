6ec116db956e2be7ed037726aaf715ca81584ac8
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2024-04-03 22:31:20 +0530

    learn how to add template

--- 01.Node-fram/index.js
+++ 01.Node-fram/index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const http = require("http");
-const path = require("path");
 const url = require("url");
 /**   
       //////////////////////////////////////////////
@@ -26,21 +25,64 @@ const url = require("url");
         // });
 */
 //////////////////////////////////////////////
-/////----Server
+/////////-------Server-------------------/////////
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+/** json data */
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+/**
+ *  server
+ */
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  /** Overview page */
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, { "content-type": "text/html" });
+
+    const cardHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHTML);
+    res.end(output);
+
+    /** api */
   } else if (pathName === "/api") {
     res.writeHead(200, { "content-type": "application/json" });
-    res.end(data);
+
+    /** Product */
   } else if (pathName === "/product") {
     res.end("This is the product");
   } else {
+    /** NOT found */
     res.writeHead(404, {
       "content-type": "text/html",
       "my-own-header": "Hello World ",
